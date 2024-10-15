7bc38efbdeaabdabbf71cfe1497fd626a0f4067a
gautier hennebert <gautierhennebert17@gmail.com>
2024-10-15 11:48:23 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dino from 'react-chrome-dino';
+
+function App() {
+  return (
+      <div>
+        <h1>Mon Jeu Chrome Dino</h1>
+        <Dino />
+      </div>
+  );
+}
+
+export default App;
